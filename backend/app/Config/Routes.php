16fc66d6a4617e84d969a351e1bf0b1a82198ae7
Your Full Name <your@email.com>
2026-03-28 16:11:51 +0530

    fix: explicit root routing and reinforced database environment mapping

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -5,6 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->get('/', 'Home::index');
 $routes->options('(:any)', 'Home::index');
 
 $routes->group('api', function($routes) {
